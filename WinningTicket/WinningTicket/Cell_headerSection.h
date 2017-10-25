89ec08be1893470eebf77e686df08862c52d564a
JayaSabeen <aj.sabeen@gmail.com>
2017-10-25 13:30:52 +0530

    Feed back 1

---
+++ WinningTicket/WinningTicket/Cell_headerSection.h
@@ -0,0 +1,13 @@
+//
+//  Cell_headerSection.h
+//  WinningTicket
+//
+//  Created by Test User on 17/10/17.
+//  Copyright © 2017 Test User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Cell_headerSection : UITableViewCell
+
+@end
